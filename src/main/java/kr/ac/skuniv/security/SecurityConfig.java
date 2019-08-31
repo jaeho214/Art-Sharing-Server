c8ab887f1a05434195470fe84292da5d67ae035a
jaeho214 <hyeonpig@gmail.com>
2019-08-31 18:43:34 +0900

    Member 기능 구현 완료

--- src/main/java/kr/ac/skuniv/security/SecurityConfig.java
+++ src/main/java/kr/ac/skuniv/security/SecurityConfig.java
@@ -1,7 +1,6 @@
 package kr.ac.skuniv.security;
 
 import kr.ac.skuniv.service.member.UserDetailsServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
