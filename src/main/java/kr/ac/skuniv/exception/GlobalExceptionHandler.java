517cd8b2c084a18558da105fd9830a8467463ce6
Hyeon Jae Ho <hyeonpig@gmail.com>
2019-09-11 00:17:58 +0900

    로거 추가

--- src/main/java/kr/ac/skuniv/exception/GlobalExceptionHandler.java
+++ src/main/java/kr/ac/skuniv/exception/GlobalExceptionHandler.java
@@ -1,8 +1,12 @@
 package kr.ac.skuniv.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 // TODO : 예외 처리 할거야
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 }
