13a6c0af12ad05802eaa756ab2bf769880eb02c6
jaeho214 <jaeho214@naver.com>
2019-09-11 00:31:33 +0900

    LogoutSuccessHandlerCustom 추가

--- src/main/java/kr/ac/skuniv/security/LogoutSuccessHandlerCustom.java
+++ src/main/java/kr/ac/skuniv/security/LogoutSuccessHandlerCustom.java
@@ -1,5 +1,7 @@
 package kr.ac.skuniv.security;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
@@ -9,8 +11,11 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class LogoutSuccessHandlerCustom implements LogoutSuccessHandler {
+    private static final Logger logger = LoggerFactory.getLogger(AccessDeniedHandlerCustom.class);
+
     @Override
     public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-        
+        logger.info("Logout Successfully!!");
+        response.sendError(HttpServletResponse.SC_OK);
     }
 }
