a769999b095bbb82d2ac261b11d1abba176100d6
jaeho214 <hyeonpig@gmail.com>
2019-09-10 23:55:00 +0900

    예외처리 클래스 생성

---
+++ src/main/java/kr/ac/skuniv/exception/GlobalExceptionHandler.java
@@ -0,0 +1,8 @@
+package kr.ac.skuniv.exception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+// TODO : 예외 처리 할거야
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+}
