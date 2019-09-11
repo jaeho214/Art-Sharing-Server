cb117cb39128b92b84f0c192cbcac6a21afd698b
jaeho214 <jaeho214@naver.com>
2019-09-12 00:46:28 +0900

    예외처리 기능 추가

--- src/main/java/kr/ac/skuniv/exception/UserDefineException.java
+++ src/main/java/kr/ac/skuniv/exception/UserDefineException.java
@@ -5,9 +5,16 @@ import lombok.Getter;
 @Getter
 public class UserDefineException extends RuntimeException {
 
+	private String originalErrorMessage;
+	private String errorMethod;
 	//예외처리
 	public UserDefineException(String message) {
 		super(message);
+	}
+
+	public UserDefineException(String message, String originalErrorMessage){
+		super(message);
+		this.originalErrorMessage = originalErrorMessage;
 	}
 
 }
