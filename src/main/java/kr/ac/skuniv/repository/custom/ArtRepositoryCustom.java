8c37fded962a6e20e849141be45fa7db49893201
jaeho214 <jaeho214@naver.com>
2019-09-19 16:24:49 +0900

    paging 처리 진행중

--- src/main/java/kr/ac/skuniv/repository/custom/ArtRepositoryCustom.java
+++ src/main/java/kr/ac/skuniv/repository/custom/ArtRepositoryCustom.java
@@ -1,9 +1,11 @@
 package kr.ac.skuniv.repository.custom;
 
 import kr.ac.skuniv.domain.dto.ArtDto;
+import org.springframework.data.domain.Page;
 
 import java.util.List;
 
 public interface ArtRepositoryCustom {
     List<ArtDto> searchArt(String searchKeyword);
+    Page<ArtDto> getArts(int pageNum);
 }
--- src/main/java/kr/ac/skuniv/repository/impl/ArtRepositoryImpl.java
+++ src/main/java/kr/ac/skuniv/repository/impl/ArtRepositoryImpl.java
@@ -1,10 +1,15 @@
 package kr.ac.skuniv.repository.impl;
 
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQuery;
 import kr.ac.skuniv.domain.dto.ArtDto;
 import kr.ac.skuniv.domain.entity.Art;
 import kr.ac.skuniv.domain.entity.QArt;
 import kr.ac.skuniv.repository.custom.ArtRepositoryCustom;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 import org.springframework.stereotype.Component;
 
@@ -23,13 +28,29 @@ public class ArtRepositoryImpl extends QuerydslRepositorySupport implements ArtR
         super(Art.class);
     }
 
+    private final int DEFAULT_LIMIT_SIZE = 9;
+
     @Override
     public List<ArtDto> searchArt(String searchKeyword) {
         JPAQuery<ArtDto> jpaQuery = new JPAQuery<>(entityManager);
-        jpaQuery.select(art)
+        jpaQuery.select(Projections.constructor(ArtDto.class, art.artName, art.member, art.price, art.rentCheck, art.member.name))
                 .from(art)
-                .where(art.artName.contains(searchKeyword));
+                .where(art.artName.contains(searchKeyword).or(art.member.name.contains(searchKeyword)))
+                .orderBy(art.id.desc())
+                ;
         List<ArtDto> arts = jpaQuery.fetch();
         return arts;
+    }
+
+    @Override
+    public Page<ArtDto> getArts(int pageNum) {
+        JPAQuery<ArtDto> jpaQuery = new JPAQuery<>(entityManager);
+        jpaQuery.select(Projections.constructor(ArtDto.class, art.artName, art.member, art.price, art.rentCheck, art.member.name))
+                .from(art)
+                .orderBy(art.id.desc())
+                .offset(--pageNum * DEFAULT_LIMIT_SIZE)
+                .limit(DEFAULT_LIMIT_SIZE);
+        List<ArtDto> arts = jpaQuery.fetch();
+        return new PageImpl<>(arts, PageRequest.of(pageNum, DEFAULT_LIMIT_SIZE, new Sort(Sort.Direction.DESC, "id")),arts.size());
     }
 }
