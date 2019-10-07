a53ca99ed39616f319c625238eaa85a185bfd7c7
jaeho214 <jaeho214@naver.com>
2019-10-07 15:41:29 +0900

    코드 수정

--- src/main/java/kr/ac/skuniv/artsharing/domain/dto/art/ArtGetDetailDto.java
+++ src/main/java/kr/ac/skuniv/artsharing/domain/dto/art/ArtGetDetailDto.java
@@ -12,21 +12,22 @@ import java.util.List;
 
 @Getter @Setter
 @NoArgsConstructor @AllArgsConstructor
-@Builder
 public class ArtGetDetailDto {
 
     private Long id; //작품 등록 번호
     private String artName; //작품 이름
     private String price; //하루당 가격
-    private boolean isRent = false; // TODO : 대여를 하게 되면 true로 바꿔주기
+    private boolean isRent; // TODO : 대여를 하게 되면 true로 바꿔주기
     private String explanation; //설명
 
     @JsonDeserialize(using = LocalDateConfig.class)
     private LocalDate regDate;
     private String artistName;
-    private List<ReplyGetDto> replyList = new ArrayList<>();
     private String imageUrl;
 
+    private List<ReplyGetDto> replyList = new ArrayList<>();
+
+    @Builder
     public ArtGetDetailDto(Long id, String artName, String price, boolean isRent, String explanation, LocalDate regDate, String artistName, String imageUrl) {
         this.id = id;
         this.artName = artName;
--- src/main/java/kr/ac/skuniv/artsharing/domain/dto/art/ArtGetDto.java
+++ src/main/java/kr/ac/skuniv/artsharing/domain/dto/art/ArtGetDto.java
@@ -10,6 +10,6 @@ public class ArtGetDto {
     private String artName; //작품 이름
     private String price; //하루당 가격
     private String userId;
-    private boolean isRent = false; // TODO : 대여를 하게 되면 true로 바꿔주기
+    private boolean isRent; // TODO : 대여를 하게 되면 true로 바꿔주기
     private String imageUrl;
 }
