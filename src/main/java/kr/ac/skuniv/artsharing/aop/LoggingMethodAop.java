9f56ff1f9b9928b690ad29b94826900f253be698
jaeho214 <jaeho214@naver.com>
2020-04-07 00:38:07 +0900

    refactoring ( add aop )

--- src/main/java/kr/ac/skuniv/artsharing/aop/LoggingMethodAop.java
+++ src/main/java/kr/ac/skuniv/artsharing/aop/LoggingMethodAop.java
@@ -6,7 +6,6 @@ import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-//TODO : 메소드 이름이 실행될 때 메소드 이름 출력을 aop로 적용
 
 @Aspect
 @Component
